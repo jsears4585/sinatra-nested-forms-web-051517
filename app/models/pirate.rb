8508bdf5b363f3cb95a2af27bc77583787de6540
Jason Sears <jsears4585@gmail.com>
2017-06-06 15:22:27 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,21 @@
 class Pirate
+  @@PIRATES = []
+
+  attr_accessor :name, :weight, :height
+
+  def initialize args
+    @name = args[:name]
+    @weight = args[:weight]
+    @height = args[:height]
+    @@PIRATES << self
+  end
+
+  def self.all
+    @@PIRATES
+  end
+
+  def self.clear
+    @@PIRATES = []
+  end
+
 end
